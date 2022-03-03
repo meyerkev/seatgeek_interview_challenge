b6d6bb95da2e7b906a082fb184af682abfd09b6a
Kevin Meyer <meyerkev248@gmail.com>
2022-03-02 21:18:32 -0500

    Adding the server itself, which works locally

---
+++ src/backend.py
@@ -0,0 +1,108 @@
+"""
+Our backend server and implicit database
+"""
+from enum import Enum
+import logging
+import threading
+
+# CR: Should these be merged?
+class SeatStatus(Enum):
+    """
+    For QUERY commands, the service should return:
+    * FREE if the queried seat hasn't been previously reserved or bought
+    * RESERVED if the seat has been reserved but not yet bought, 
+    * SOLD when the seat has already been bought 
+    """
+    FREE = 1
+    RESERVED = 2
+    SOLD = 3
+
+class ReturnStatus(Enum):
+    OK = 4
+    FAIL = 5
+
+class Seat():
+    def __init__(self, seat_id, status):
+        self._seat_id = seat_id
+        self._status = status
+        # CR: This is a lot of locks.  Discuss alternate strategies (DB-level?)
+        # TODO: Convert to read-write lock ala https://gist.github.com/tylerneylon/a7ff6017b7a1f9a506cf75aa23eacfd6
+        self._lock = threading.Lock()
+
+    @property
+    def status(self):
+        with self._lock:
+            return self._status
+
+    def reserve(self):
+        """
+        For RESERVE commands, the service must return:
+        * OK if the seat was previously FREE
+        * FAIL otherwise
+        """
+        with self._lock:
+            # Avoid using self.status while holding the lock!
+            if self._status != SeatStatus.FREE:
+                return ReturnStatus.FAIL
+            self._status = SeatStatus.RESERVED
+            return ReturnStatus.OK
+
+    def buy(self):
+        """
+        For BUY commands, the service must return:
+        * OK if the service was previously marked as RESERVED.
+        * FAIL otherwise 
+        """
+        with self._lock:
+            if self._status != SeatStatus.RESERVED:
+                return ReturnStatus.FAIL
+            self._status = SeatStatus.SOLD
+            return ReturnStatus.OK
+
+
+# This would literally be a DB, so I don't feel awkward about it being a global
+# Alt. would be singleton, especially if we use non-seat-level locking
+SEAT_DB = {}
+
+def take_action(action, seat_id):
+    """
+    Returns a status
+
+    The server should return responses according to these rules: 
+    * We assume that any seat that has not been reserved or bought exists and is FREE 
+    * For RESERVE commands, the service must return OK if the seat was previously FREE. If the seat was in any other state, the response should be FAIL
+    * For BUY commands, the service must return OK if the service was previously marked as RESERVED. If the seat was in any other state, the response should be FAIL 
+    * For QUERY commands, the service should return FREE if the queried seat hasn't been previously reserved or bought, RESERVED if the seat has been reserved but not yet bought, and SOLD when the seat has already been bought
+    * The service should return FAIL for any unknown or invalid message it receives
+    """
+    # Possible TODO: Can we do something interesting with 
+    # collections.defaultdict constructors?
+    seat = SEAT_DB.get(seat_id)
+    if seat is None:
+        seat = Seat(seat_id, SeatStatus.FREE)
+        SEAT_DB[seat_id] = seat
+
+    if action == "QUERY":
+        # SELECT status FROM seats WHERE seat_id==<seat_id>
+        return seat.status
+    elif action == "RESERVE":
+        return seat.reserve()
+    elif action == "BUY": 
+        return seat.buy()
+    
+    logging.info(f"Invalid action {action}")
+    return ReturnStatus.FAIL
+
+
+def process_message(message):
+    # This could be done with regex, but let's just run a split
+    # Input string is ACTION: seat\n
+    logging.debug(f"Message: {message}")
+    try:
+        action, seat = message.split(": ")
+        seat_id = seat.strip()
+    except ValueError:  # No : , so list is of len 1 and cannot be split
+        logging.info(f"Message {message} could not be parsed")
+        return ReturnStatus.FAIL.name
+
+    return take_action(action, seat).name
---
+++ src/server.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+import logging
+import socket
+import threading
+
+from time import sleep
+
+import backend
+
+LISTEN_PORT = 8099
+ENCODING = 'utf-8'
+
+class SocketLineReader:
+    def __init__(self, socket):
+        self.socket = socket
+        self._buffer = b''
+
+    def readline(self):
+        pre, separator, post = self._buffer.partition(b'\n')
+        if separator:
+            self._buffer = post
+            return pre + separator
+
+        while True:
+            data = self.socket.recv(1024)
+            if not data:
+                return None
+
+            pre, separator, post = data.partition(b'\n')
+            if not separator:
+                self._buffer += data
+            else:
+                data = self._buffer + pre + separator
+                self._buffer = post
+                return data
+
+
+def start_server(socket):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # Sadly required to force-flush tiny messages like these
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+    sock.bind(('', LISTEN_PORT))
+    sock.listen(0)
+
+    def handle(conn):
+        logging.info('connected:', addr)
+
+        reader = SocketLineReader(conn)
+        while True:
+            data = reader.readline()
+            if not data:
+                break
+            message = data.decode(ENCODING)
+            status = backend.process_message(message)
+            # Do not forget this \n.
+            # The test server won't notice messages until you send a \n
+            send_bytes = status.encode(ENCODING) + b'\n'
+            conn.send(send_bytes)
+            logging.debug(send_bytes)
+
+        conn.close()
+
+    try:
+        while True:
+            conn, addr = sock.accept()
+            threading.Thread(target=handle, args=(conn,)).start()
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    start_server(socket=socket)
